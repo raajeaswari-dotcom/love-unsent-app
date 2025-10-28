b2e297b4a4d5fc9ce65574e3896cdf742620f750
stallioninet <78951510+stallioninet@users.noreply.github.com>
2025-10-28 16:47:31 +0530

    Fix routing for Render and add _redirects file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import your admin pages
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminLoginPage from "./pages/AdminLoginPage";
 import AdminDashboardPage from "./pages/AdminDashboardPage";
 
-// You can add your other imports (like HomePage, etc.) here
-
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        {/* Public routes */}
-        {/* Example: <Route path="/" element={<HomePage />} /> */}
-
-        {/* Admin routes */}
         <Route path="/love-office" element={<AdminLoginPage />} />
         <Route path="/love-office/dashboard" element={<AdminDashboardPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
