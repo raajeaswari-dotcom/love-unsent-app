9661d98f6b1e25320d2898d3ba6b3a1570d4df25
stallioninet <78951510+stallioninet@users.noreply.github.com>
2025-10-28 15:45:11 +0530

    Add admin routes to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Import your admin pages
+import AdminLoginPage from "./pages/AdminLoginPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
 
-// Admin routes added by merge
-/* Admin routes are available at /love-office and /love-office/dashboard */
+// You can add your other imports (like HomePage, etc.) here
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        {/* Example: <Route path="/" element={<HomePage />} /> */}
+
+        {/* Admin routes */}
+        <Route path="/love-office" element={<AdminLoginPage />} />
+        <Route path="/love-office/dashboard" element={<AdminDashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
